073497186ff2e5f4ecc87e04566f1728a57e7f2c
Amine El Achari <amineachari97@gmail.com>
2023-03-20 11:18:03 +0000

    first commit

---
+++ Frontend/src/pages/Calendrier.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Container from '../Components/Container/Container'
+
+const Calendrier = () => {
+  return (
+    <Container>Calendrier</Container>
+  )
+}
+
+export default Calendrier
---
+++ Frontend/src/pages/Patients.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from '../Components/Container/Container'
+
+const Patients = () => {
+  return (
+    <Container>
+      Patients
+    </Container>
+  )
+}
+
+export default Patients
